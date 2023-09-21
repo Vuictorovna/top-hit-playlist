ba35b87ca267804c7385f7c8d1804840e722fc92
Volha Sakharevich <vuictorovna@gmail.com>
2023-09-22 00:23:37 +0100

    Add more functionality to transform_data function

--- dags/transformation.py
+++ dags/transformation.py
@@ -3,5 +3,9 @@ import pandas as pd
 
 def transform_data():
     df = pd.read_csv("files/playlist_2010to2022.csv")
-    df = df.dropna()  # Remove missing values
+    df = df.dropna()
+    df["duration_mins"] = (df["duration_ms"] / (1000 * 60)).round(2)
+    df.drop_duplicates(inplace=True)
+    df["track_name"] = df["track_name"].str.lower().str.replace("[^a-z0-9 ]", "")
+
     df.to_csv("files/clean_data.csv", index=False)
